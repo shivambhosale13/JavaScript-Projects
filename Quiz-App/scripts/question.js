72875c21592270f708723b7017d5ecf8dab57658
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-11 14:02:31 +0530

    added questions data

---
+++ Quiz-App/scripts/question.js
@@ -0,0 +1,36 @@
+const questions = [
+  {
+    question : 'Which is largest animal in the world?',
+    answer : [
+      {text : "Shark" , correct : false},
+      {text : "Blue Whale" , correct : true},
+      {text : "Elephant" , correct : false},
+      {text : "Giraffe" , correct : false}
+    ]
+  },
+  {
+    question : 'Which is smallest country in the world?',
+    answer : [
+      {text : "Vatican City " , correct : true},
+      {text : "Bhutan" , correct : false},
+      {text : "Nepal" , correct : false},
+      {text : "Shri Lanka" , correct : false}
+    ]
+  },{
+    question : 'Which is largest desert in the world?',
+    answer : [
+      {text : "Gobi" , correct : false},
+      {text : "Kahari" , correct : false},
+      {text : "Sahara" , correct : true},
+      {text : "Anatarctica" , correct : false}
+    ]
+  },{
+    question : 'Which is smallest contient in the world?',
+    answer : [
+      {text : "Asia" , correct : false},
+      {text : "Austrilia" , correct : true},
+      {text : "Asia" , correct : true},
+      {text : "Africa" , correct : false}
+    ]
+  }
+]
