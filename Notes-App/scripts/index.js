4445ba0c966cf818f69b41c787b765fdfa526ba5
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-14 13:14:43 +0530

    added loaclstroage and made delete button interactive

--- Notes-App/scripts/index.js
+++ Notes-App/scripts/index.js
@@ -1,18 +1,56 @@
 let createButton = document.querySelector('.js-create-button');
 
-let inputBox = document.querySelector('.js-input-box')
+let notesHtml = localStorage.getItem('notes') || '';
 
-let notesHtml = '';
+document.querySelector('.js-notes-input').innerHTML = notesHtml;
 
-createButton.addEventListener('click' , () =>{
+addDeleteEvents();
+addInputEvents();
+
+createButton.addEventListener('click', () => {
 
   notesHtml += `
-    <div class="input-box js-input-box">
-      <p contenteditable="true"></p>
-      <img src="images/delete.png" alt="" class="delete-button ">
+    <div class="input-box js-input-box"> 
+      <p contenteditable="true"></p> 
+      <img src="images/delete.png" alt="" class="delete-button js-delete-button"> 
     </div>
-  `
-  document.querySelector('.js-notes-input')
-  .innerHTML = notesHtml;
+  `;
+
+  document.querySelector('.js-notes-input').innerHTML = notesHtml;
+
+  localStorage.setItem('notes', notesHtml);
+
+  addDeleteEvents();
+  addInputEvents()
 });
 
+
+function addDeleteEvents() {
+
+  document.querySelectorAll('.js-delete-button').forEach((deleteButton) => {
+
+    deleteButton.addEventListener('click', () => {
+
+      deleteButton.parentElement.remove();
+
+      notesHtml = document.querySelector('.js-notes-input').innerHTML;
+
+      localStorage.setItem('notes', notesHtml);
+
+    });
+
+  });
+}
+
+function addInputEvents() {
+
+  document.querySelectorAll('.js-input-box p').forEach((note) => {
+
+    note.addEventListener('input', () => {
+      notesHtml = document.querySelector('.js-notes-input').innerHTML;
+      localStorage.setItem('notes', notesHtml);
+    });
+
+  });
+
+}
