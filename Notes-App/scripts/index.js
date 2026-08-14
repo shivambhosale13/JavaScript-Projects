de81b2b8c1923d55ad3db15c8db2ab64d2935d83
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-14 12:52:39 +0530

    made so that create notes button can create multiple notes input

--- Notes-App/scripts/index.js
+++ Notes-App/scripts/index.js
@@ -2,7 +2,17 @@ let createButton = document.querySelector('.js-create-button');
 
 let inputBox = document.querySelector('.js-input-box')
 
+let notesHtml = '';
 
 createButton.addEventListener('click' , () =>{
-  inputBox.classList.add('input-box-visible')
-})
+
+  notesHtml += `
+    <div class="input-box js-input-box">
+      <p contenteditable="true"></p>
+      <img src="images/delete.png" alt="" class="delete-button ">
+    </div>
+  `
+  document.querySelector('.js-notes-input')
+  .innerHTML = notesHtml;
+});
+
