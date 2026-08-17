312c856e133ae5a92075e8dd1c1fa777df60430e
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-17 18:45:43 +0530

    added eventlister to the calculate button and added the dayjs libarary

---
+++ Age-Calculator-App/index.js
@@ -0,0 +1,12 @@
+let calculateButton = document.querySelector('.js-calculate-button')
+
+let date = '';
+
+calculateButton.addEventListener('click', () => {
+    date = document.querySelector('.js-date').value;
+    //calculateAge();
+});
+
+//function calculateAge() {
+// 
+//}
