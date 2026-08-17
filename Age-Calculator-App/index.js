21bec23a9c6a932ff7a6b3b5d832e9fcdc5efc1d
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-17 19:31:20 +0530

    added calculate age function and displayed the age on the web page

--- Age-Calculator-App/index.js
+++ Age-Calculator-App/index.js
@@ -4,9 +4,22 @@ let date = '';
 
 calculateButton.addEventListener('click', () => {
     date = document.querySelector('.js-date').value;
-    //calculateAge();
+    calculateAge(date);
 });
 
-//function calculateAge() {
-// 
-//}
+
+function calculateAge(date) {
+    const birthday = dayjs(date);
+    const today = dayjs();
+
+    const years = today.diff(birthday, 'year');
+    const afterYears = birthday.add(years, 'year');
+
+    const months = today.diff(afterYears, 'month');
+    const afterMonths = afterYears.add(months, 'month');
+
+    const days = today.diff(afterMonths, 'day');
+
+   document.querySelector('.js-display-age')
+   .innerText = `You are ${years} years, ${months} months and ${days} days old`
+}
